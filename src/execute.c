41fc96c3abb7593255271313f55b02b7a5239814
Fred Charbonnier <frederickcharbonnier@icloud.com>
2025-05-02 16:35:18 +0200

    cleaned up some norm

--- src/execute.c
+++ src/execute.c
@@ -26,15 +26,12 @@ char	*access_full_path(char **paths, char *cmd)
 	i = 0;
 	while (paths[i])
 	{
-		// ft_printf("Path |%d|: %s\n", i, paths[i]);
 		half_path = ft_strjoin(paths[i], "/");
 		full_path = ft_strjoin(half_path, cmd);
 		free(half_path);
-		// ft_printf("Full |%d|: %s\n", i, full_path);
 		if (access(full_path, F_OK) == 0)
 		{
-			ft_free_array(paths); // Free the split array
-			// printf("about to return |%s|\n", full_path);
+			ft_free_array(paths);
 			return (full_path);
 		}
 		free(full_path);
@@ -53,7 +50,6 @@ char	*get_full_path(char *cmd, char **ev)
 	i = 0;
 	path_env = NULL;
 	paths = NULL;
-	// fprintf(stderr, "ENVPDATA is |%s|\n", ev[0]);
 	while (ev[i])
 	{
 		if (ft_strncmp(ev[i], "PATH=", 5) == 0)
@@ -68,20 +64,11 @@ char	*get_full_path(char *cmd, char **ev)
 		fprintf(stderr, "Error finding environment paths");
 		return (NULL);
 	}
-	// Split the PATH variable into directories
 	paths = ft_split(path_env, ':');
 	if (!paths)
-	{
-		// fprintf(stderr, "WEEWEEEWEE\n");
 		error();
-	}
-	// Search for the command in each directory
-	// char *full_path = access_full_path(paths, cmd);
-	// printf("returned full path is |%s|\n", full_path);
 	return (access_full_path(paths, cmd));
 }
-
-// example of argv in this case would be "/bin/cat", "filename.txt", NULL
 
 int	execute_cmd(char *av, char **ev, int input_fd, int output_fd)
 {
@@ -89,45 +76,26 @@ int	execute_cmd(char *av, char **ev, int input_fd, int output_fd)
 	char	**argv;
 	pid_t	pid;
 
-	// Debugging: Print file descriptors
-	fprintf(stderr, "execute_cmd: input_fd=%d, output_fd=%d\n", input_fd, output_fd);
-
 	argv = ft_split(av, ' ');
 	if (!argv)
 		error();
 	path = get_full_path(argv[0], ev);
 	if (!path)
-	{
-		ft_free_array(argv);
-		print_err("Command not found", av, EXIT_FAILURE);
-		return (EXIT_FAILURE);
-	}
+		return (ft_free_array(argv), print_err("Command not found, ", av, -1));
 	pid = fork();
 	if (pid == -1)
-	{
-		ft_free_array(argv);
-		free(path);
-		error();
-	}
+		return (ft_free_array(argv), free(path), error(), -1);
 	if (pid == 0)
 	{
-		// In child process: redirect input/output
-		if (dup2(input_fd, STDIN_FILENO) == -1 || dup2(output_fd, STDOUT_FILENO) == -1)
+		if (dup2(input_fd, 0) == -1 || dup2(output_fd, 1) == -1)
 			error();
-		// Close unused file descriptors
 		close(input_fd);
 		close(output_fd);
-		// Execute the command
 		if (execve(path, argv, ev) == -1)
-		{
-			perror("execve failed");
-			exit(EXIT_FAILURE);
-		}
+			return (perror("execve failed"), exit(EXIT_FAILURE), -1);
 	}
-	// In parent process: clean up
 	ft_free_array(argv);
 	free(path);
-	// Parent does not close input_fd/output_fd here; caller handles it
 	waitpid(pid, NULL, 0);
 	return (EXIT_SUCCESS);
 }
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: fredchar <fredchar@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/05 15:06:22 by fredchar          #+#    #+#             */
-/*   Updated: 2025/05/02 16:07:32 by fredchar         ###   ########.fr       */
+/*   Updated: 2025/05/02 16:34:38 by fredchar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,19 +21,19 @@ void	child_process(char *argv, char **envp, int *input_fd, int is_last, int file
 
 	if (!is_last && pipe(fd) == -1)
 		error();
-	output_fd = is_last ? fileout : fd[1]; // Use fileout for the final command
+	if (is_last)
+		output_fd = fileout;
+	else
+		output_fd = fd[1];
 	execute_cmd(argv, envp, *input_fd, output_fd);
-	// Close unused ends of the pipe in the parent process
 	if (!is_last)
 	{
 		close(fd[1]);
-		close(*input_fd); // Close the previous input_fd
-		*input_fd = fd[0]; // Update input_fd to the read end of the pipe
+		close(*input_fd);
+		*input_fd = fd[0];
 	}
 	else
-	{
-		close(output_fd); // Close output_fd in the parent process for the last command
-	}
+		close(output_fd);
 }
 
 int	main(int argc, char **argv, char **envp)
@@ -48,12 +48,12 @@ int	main(int argc, char **argv, char **envp)
 	i = 2;
 	fileout = open_file(argv[argc - 1], 1);
 	filein = open_file(argv[1], 2);
-	input_fd = filein; // Initialize input_fd with the input file descriptor
+	input_fd = filein;
 	while (i < argc - 2)
 	{
 		child_process(argv[i], envp, &input_fd, 0, fileout);
 		i++;
 	}
-	child_process(argv[argc - 2], envp, &input_fd, 1, fileout); // Final command
+	child_process(argv[argc - 2], envp, &input_fd, 1, fileout);
 	close(fileout);
 }
